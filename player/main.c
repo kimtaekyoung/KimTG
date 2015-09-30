446d24e7f16c396f9e8fde46e7dc3d8ef3e4aca4
Kim TG <xoriddl@naver.com>
2015-09-30 00:00:21 -0700

    github -1st-

--- player/main.c
+++ player/main.c
@@ -5,6 +5,7 @@ void main(){
 	printf("hello bug123\n");
 	printf("이번엔 main이라능!!!!!\n");
 	func2();
+	printf("github -_-");
 	printf("hello.... merge123 3\n");
 	func1();
 	printf("hello bug123 2\n");

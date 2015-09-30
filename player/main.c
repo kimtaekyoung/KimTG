2b48f7c49539df87470991bea4cffb7e480e6797
Kim TG <xoriddl@naver.com>
2015-09-29 23:37:28 -0700

    수정했어

--- player/main.c
+++ player/main.c
@@ -3,9 +3,10 @@
 
 void main(){
 	printf("hello bug123\n");
+	printf("이번엔 main이라능!!!!!\n");
 	func2();
-	printf("hello bug123 3\n");
+	printf("hello merge!!!@#!@!@#!$!%!#$%Q@#$! 3\n");
 	func1();
-	printf("hello bug123 2\n");
+	printf("hello merge!!!! 2\n");
 	return 0;
 }

18a6b8611947f9127f1931fcfcbe00986679c3f7
Kim TG <xoriddl@naver.com>
2015-09-29 23:34:52 -0700

    merge 1st

--- player/main.c
+++ player/main.c
@@ -4,8 +4,9 @@
 void main(){
 	printf("hello bug123\n");
 	func2();
-	printf("hello bug123 3\n");
+	printf("hello.... merge123 3\n");
 	func1();
 	printf("hello bug123 2\n");
+	printf("한글이라능능능 \n");
 	return 0;
 }
